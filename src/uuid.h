2081de1c9179c66bf86f2e39c8b2056ccba599cf
Matt Hughes <matth@eidetic.net.au>
2023-07-27 13:23:35 +1000

    Remove libuuid since it's only linux compatible

--- src/uuid.h
+++ src/uuid.h
@@ -2,11 +2,9 @@
 
 #include "wtf/wtf.h"
 #include <string>
-#include <uuid/uuid.h>
 
 namespace pc::uuid {
 
-const std::string generate();
 const std::string word(int32_t min_chars = 4, int32_t max_chars = 9,
                        std::string prefix = "", std::string suffix = "");
 
