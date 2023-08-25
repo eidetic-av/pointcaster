52ccbc320b4d0a632279923b8b9c5f17988db9c9
Matt Hughes <matth@eidetic.net.au>
2023-08-25 17:54:52 +1000

    Make pc::types::* serializable

--- src/structs.h
+++ src/structs.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pointclouds.h>
+#include <nlohmann/json.hpp>
 
 namespace pc::types {
 
@@ -19,26 +20,39 @@ template <typename T> struct minMax {
   }
 };
 
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(minMax<float>, min, max);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(minMax<double>, min, max);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(minMax<int>, min, max);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(minMax<unsigned int>, min, max);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(minMax<short>, min, max);
+
 struct short3 {
   short x, y, z = 0;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(short3, x, y, z);
 
 struct float3 {
   float x, y, z = 0;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(float3, x, y, z);
 
 struct float4 {
   float w, x, y, z = 0;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(float4, w, x, y, z);
 
 struct int3 {
   int x, y, z = 0;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(int3, x, y, z);
 
 // just alias these for now before refactoring bob-pointclouds library
 using PointCloud = bob::types::PointCloud;
 using position = bob::types::position;
 using color = bob::types::color;
 using uint2 = bob::types::uint2;
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(color, r, g, b, a);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(uint2, x, y);
 
 } // namespace pc::types
