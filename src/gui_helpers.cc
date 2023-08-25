36772b91f19e4c746140153922c82d267bba98ea
Matt Hughes <matth@eidetic.net.au>
2023-08-25 17:55:40 +1000

    Tree node gui helper pushes unique id

--- src/gui_helpers.cc
+++ src/gui_helpers.cc
@@ -23,14 +23,11 @@ unsigned int _parameter_index;
 void begin_gui_helpers() { _parameter_index = 0; }
 
 bool begin_tree_node(std::string_view name, bool &open) {
+  ImGui::PushID(_parameter_index++);
   auto node_flags = ImGuiTreeNodeFlags_None;
-  if (open)
-    node_flags = ImGuiTreeNodeFlags_DefaultOpen;
-  if (ImGui::TreeNodeEx(name.data(), node_flags)) {
-    open = true;
-    return true;
-  }
-  open = false;
-  return false;
+  if (open) node_flags = ImGuiTreeNodeFlags_DefaultOpen;
+  open = ImGui::TreeNodeEx(name.data(), node_flags);
+  ImGui::PopID();
+  return open;
 }
 } // namespace pc::gui
