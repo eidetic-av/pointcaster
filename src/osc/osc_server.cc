f0a73d34d7439508aa725aa478b45e4d99617d3b
Matt Hughes <matth@eidetic.net.au>
2024-02-27 21:50:52 +1100

    OSC server receiving & setting vectors

--- src/osc/osc_server.cc
+++ src/osc/osc_server.cc
@@ -17,21 +17,56 @@ using namespace pc::parameters;
 
 OscServer::OscServer(OscServerConfiguration &config) : _config(config) {
   _config.port = std::clamp(_config.port, 1024, 49151);
-  create_server(_config.port);
+  if (_config.enable) {
+    create_server(_config.port);
+  }
   declare_parameters("oscserver", _config);
 }
 
 template <typename T> void receive_msg(std::string_view address, T value) {
   if constexpr (std::is_same_v<T, std::string_view>) {
     pc::logger->debug("{}: s {}", address, value);
-  } else {
-    std::string topic{address.substr(1, -1)};
-    std::replace(topic.begin(), topic.end(), '/', '.');
-    // pc::logger->debug("trying {} to {}", topic, value);
+    return;
+  }
+  std::string topic{address.substr(1, -1)};
+  std::replace(topic.begin(), topic.end(), '/', '.');
+
+  if constexpr (types::VectorType<T>) {
+    // if the osc message is setting the entire vector
     if (parameter_bindings.contains(std::string_view{topic})) {
-      // pc::logger->debug("setting {} to {}", topic, value);
       set_parameter_value(topic, value);
     }
+  } else {
+    // if the osc message is setting individual elements of a vector
+    std::optional<int> vector_element;
+    if (strings::ends_with(topic, ".0") || strings::ends_with(topic, ".x")) {
+      vector_element = 0;
+    } else if (strings::ends_with(topic, ".1") ||
+	       strings::ends_with(topic, ".y")) {
+      vector_element = 1;
+    } else if (strings::ends_with(topic, ".2") ||
+	       strings::ends_with(topic, ".z")) {
+      vector_element = 2;
+    } else if (strings::ends_with(topic, ".3") ||
+	       strings::ends_with(topic, ".w")) {
+      vector_element = 3;
+    }
+    if (!vector_element) return;
+    topic = topic.substr(0, topic.length() - 2);
+    std::visit(
+	[&](auto &&parameter_value_ref) {
+	  auto &parameter = parameter_value_ref.get();
+	  using ParamType = std::decay_t<decltype(parameter)>;
+	  if constexpr (types::VectorType<ParamType>) {
+	    if constexpr (std::is_same_v<typename ParamType::vector_type, T>) {
+              // if the parameter we are trying to set is a vector
+              // and we are setting it with the element type of the vector,
+	      // then we can get the vector and set just the element
+	      parameter[*vector_element] = value;
+	    }
+          }
+	},
+	parameter_bindings.at(topic).value);
   }
 }
 
@@ -43,9 +78,10 @@ void OscServer::create_server(int port) {
       [](const char *path, const char *types, lo_arg **argv, int argc) {
         // if (argc > 1) {
 	if (std::strcmp(types, "f") == 0) {
-	  if (argc == 3) {
-	    receive_msg(path, Float3{argv[0]->f, argv[1]->f, argv[2]->f});
-	  }
+	  receive_msg(path, argv[0]->f);
+        }
+        if (std::strcmp(types, "fff") == 0) {
+	  receive_msg(path, Float3{argv[0]->f, argv[1]->f, argv[2]->f});
 	}
         // } else {
         //   if (std::strcmp(types, "i") == 0) {
@@ -67,11 +103,18 @@ void OscServer::draw_imgui_window() {
 
   ImGui::Begin("OSC Server", nullptr);
 
+  auto enabled = _config.enable;
+
   if (pc::gui::draw_parameters(
 	  "oscserver", struct_parameters.at(std::string{"oscserver"}))) {
 
     _config.port = std::clamp(_config.port, 1024, 49151);
-    // 
+
+    if (enabled && !_config.enable) {
+      _server_thread->stop();
+    } else if (!enabled && _config.enable) {
+      _server_thread->start();
+    }
 
   }
 
