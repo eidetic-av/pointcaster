2ce8d185777a1fb2f95d2a0940a4a348ac9682bd
Matt Hughes <matth@eidetic.net.au>
2024-04-25 17:09:57 +1000

    OrbbecSDK port changes to fix windows builds

--- ports/orbbec-sdk/portfile.cmake
+++ ports/orbbec-sdk/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
   REF "v${VERSION}"
   SHA512 72874b8bea22812aeb67fff8de01c0daaceb56112f348db522e31be7c7dc1bda2a067f166d40ceb2352076b4b7c8eeddf13f166e7d8f6eda49d432fb7069dc3c
   HEAD_REF main
+  PATCHES fix-lib-paths.patch
 )
 
 file(INSTALL
@@ -11,16 +12,18 @@ file(INSTALL
   DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
 file(INSTALL
+    ${SOURCE_PATH}/OrbbecSDKConfig.cmake
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/OrbbecSDK)
+
+file(INSTALL
   ${SOURCE_PATH}/cmake/
-  DESTINATION ${CURRENT_PACKAGES_DIR}/share/OrbbecSDK)
+  DESTINATION ${CURRENT_PACKAGES_DIR}/share/OrbbecSDK/cmake)
 
 # platform specific var setup
 
 if (VCPKG_TARGET_IS_LINUX)
-  set(LIB_EXT ".so")
   set(LIB_DIR "linux_x64")
 elseif(VCPKG_TARGET_IS_WINDOWS)
-  set(LIB_EXT ".dll")
   set(LIB_DIR "win_x64")
 else()
   message(FATAL_ERROR "Platform not supported.")
@@ -29,20 +32,20 @@ endif()
 # the depth engine library will come from the k4a repo instead,
 # which works with both k4a and orbbec devices. So skip it for this port
 
-file(GLOB_RECURSE LIB_FILES "${SOURCE_PATH}/lib/${LIB_DIR}/*${LIB_EXT}*")
+file(GLOB_RECURSE LIB_FILES "${SOURCE_PATH}/lib/${LIB_DIR}/*")
 
 foreach(LIB_FILE ${LIB_FILES})
   get_filename_component(FILE_NAME ${LIB_FILE} NAME)
   if (NOT FILE_NAME MATCHES "depthengine")
-    file(INSTALL ${LIB_FILE}
-      DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    if(FILE_NAME MATCHES "dll")
+      file(INSTALL ${LIB_FILE}
+        DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    else()
+      file(INSTALL ${LIB_FILE}
+        DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    endif()
   endif()
 endforeach()
-
-configure_file(
-    ${CURRENT_PACKAGES_DIR}/share/OrbbecSDK/OrbbecSDKConfig.cmake.in
-    ${CURRENT_PACKAGES_DIR}/share/OrbbecSDK/OrbbecSDKConfig.cmake
-    @ONLY)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt 
   DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
