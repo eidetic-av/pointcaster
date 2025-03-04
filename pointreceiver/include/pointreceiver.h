118417b002aa443d5d27490f62a4ce5cdc60ac25
Matt Hughes <matth@eidetic.net.au>
2025-03-04 16:35:47 +1100

    Move pointreceiver to a more standard C api for inclusion in other projects

---
+++ pointreceiver/include/pointreceiver.h
@@ -0,0 +1,137 @@
+/**
+ * @file pointreceiver.h
+ * @brief Public API for the PointReceiver library.
+ *
+ * This header provides a C API for the PointReceiver library,
+ * which handles parameter updates and point cloud streams from a
+ * local or remote Pointcaster instance.
+ */
+
+#pragma once
+#ifndef POINTRECEIVER_H
+#define POINTRECEIVER_H
+
+#ifndef __cplusplus
+#include <stdbool.h>
+#endif
+
+#ifdef __ANDROID__
+#include <jni.h>
+#define PR_EXPORT JNIEXPORT
+#elif _WIN32
+#define PR_EXPORT __declspec(dllexport)
+#else
+#define PR_EXPORT
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Opaque structure representing the PointReceiver context.
+ *
+ * The internal details of this structure are hidden from API users.
+ */
+typedef struct pointreceiver_context pointreceiver_context;
+
+/**
+ * @brief Enumerates the types of messages that can be received.
+ */
+typedef enum {
+  POINTRECEIVER_MSG_TYPE_CONNECTED = 0,           /**< Connected message */
+  POINTRECEIVER_MSG_TYPE_CLIENT_HEARTBEAT,          /**< Client heartbeat */
+  POINTRECEIVER_MSG_TYPE_CLIENT_HEARTBEAT_RESPONSE, /**< Response to client heartbeat */
+  POINTRECEIVER_MSG_TYPE_PARAMETER_UPDATE,          /**< Parameter update message */
+  POINTRECEIVER_MSG_TYPE_PARAMETER_REQUEST,         /**< Parameter request message */
+  POINTRECEIVER_MSG_TYPE_UNKNOWN                    /**< Unknown message type */
+} pointreceiver_message_type;
+
+/**
+ * @brief Enumerates the possible types for parameter values.
+ */
+typedef enum {
+  POINTRECEIVER_PARAM_VALUE_FLOAT = 0,   /**< Float value */
+  POINTRECEIVER_PARAM_VALUE_INT,         /**< Integer value */
+  POINTRECEIVER_PARAM_VALUE_FLOAT3,      /**< 3D float vector */
+  POINTRECEIVER_PARAM_VALUE_ARRAY3F,       /**< Array of 3 floats */
+  POINTRECEIVER_PARAM_VALUE_UNKNOWN        /**< Unknown parameter type */
+} pointreceiver_param_value_type;
+
+/**
+ * @brief Structure representing a synchronized message.
+ *
+ * This structure encapsulates the message type, an identifier, and a union
+ * holding the associated value.
+ */
+typedef struct {
+  pointreceiver_message_type message_type;         /**< Type of the message */
+  char id[256];                                    /**< Identifier string */
+  pointreceiver_param_value_type value_type;       /**< Type of the value contained in the union */
+  union {
+    float float_val;                               /**< Float value */
+    int int_val;                                   /**< Integer value */
+    struct {
+      float x, y, z;                               /**< 3D float vector */
+    } float3_val;
+  } value;                                         /**< Union holding the message value */
+} pointreceiver_sync_message;
+
+/**
+ * @brief Creates a new PointReceiver context.
+ *
+ * @return A pointer to a new pointreceiver_context, or NULL on failure.
+ */
+PR_EXPORT pointreceiver_context *pointreceiver_create_context(void);
+
+/**
+ * @brief Destroys a PointReceiver context.
+ *
+ * Frees resources associated with the given context.
+ *
+ * @param ctx Pointer to the context to be destroyed.
+ */
+PR_EXPORT void pointreceiver_destroy_context(pointreceiver_context *ctx);
+
+/**
+ * @brief Starts the message receiver thread.
+ *
+ * This function initialises the message receiving mechanism and begins
+ * listening for messages from a specified pointcaster address.
+ *
+ * @param ctx Pointer to the PointReceiver context.
+ * @param pointcaster_address The network address of the pointcaster.
+ * @return 0 on success, non-zero on failure.
+ */
+PR_EXPORT int pointreceiver_start_message_receiver(pointreceiver_context *ctx,
+                                     const char *pointcaster_address);
+
+/**
+ * @brief Stops the message receiver thread.
+ *
+ * Stops listening for messages and terminates the associated thread.
+ *
+ * @param ctx Pointer to the PointReceiver context.
+ * @return 0 on success, non-zero on failure.
+ */
+PR_EXPORT int pointreceiver_stop_message_receiver(pointreceiver_context *ctx);
+
+/**
+ * @brief Dequeues a message from the message queue.
+ *
+ * Attempts to dequeue a message within a specified timeout period.
+ *
+ * @param ctx Pointer to the PointReceiver context.
+ * @param[out] out_message Pointer to a structure where the dequeued message will be stored.
+ * @param timeout_ms Timeout in milliseconds to wait for a message.
+ * @return true if a message was successfully dequeued, false otherwise.
+ */
+PR_EXPORT bool pointreceiver_dequeue_message(pointreceiver_context *ctx,
+                              pointreceiver_sync_message *out_message,
+                              int timeout_ms);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // POINTRECEIVER_H
--- pointreceiver/src/pointreceiver.cc
+++ pointreceiver/src/pointreceiver.cc
@@ -1,5 +1,6 @@
-#include "pointreceiver.h"
+#include "../include/pointreceiver.h"
 #include "../../src/client_sync/updates.h"
+
 #include <chrono>
 #include <cstring>
 #include <execution>
@@ -15,6 +16,13 @@
 #include <zmq.hpp>
 #include <zpp_bits.h>
 
+#ifdef __ANDROID__
+#include <android/log.h>
+#else
+#include <spdlog/spdlog.h>
+#endif
+
+
 static constexpr int pointcloud_stream_port = 9992;
 static constexpr int message_stream_port = 9002;
 
@@ -29,44 +37,57 @@ static moodycamel::BlockingReaderWriterCircularBuffer<PointCloud>
 
 std::unique_ptr<std::jthread> pointcloud_thread;
 
-struct PointReceiverContext {
+struct pointreceiver_context {
   moodycamel::BlockingReaderWriterQueue<SyncMessage> message_queue;
   std::unique_ptr<std::jthread> message_thread;
 
-  PointReceiverContext() : message_queue() {}
+  pointreceiver_context() : message_queue() {}
 };
 
-static PointReceiverMessageType
+static pointreceiver_message_type
 convertMessageType(pc::client_sync::MessageType message_type) {
   switch (message_type) {
-  case pc::client_sync::MessageType::Connected: return MSG_TYPE_CONNECTED;
+  case pc::client_sync::MessageType::Connected: return POINTRECEIVER_MSG_TYPE_CONNECTED;
   case pc::client_sync::MessageType::ClientHeartbeat:
-    return MSG_TYPE_CLIENT_HEARTBEAT;
+    return POINTRECEIVER_MSG_TYPE_CLIENT_HEARTBEAT;
   case pc::client_sync::MessageType::ClientHeartbeatResponse:
-    return MSG_TYPE_CLIENT_HEARTBEAT_RESPONSE;
+    return POINTRECEIVER_MSG_TYPE_CLIENT_HEARTBEAT_RESPONSE;
   case pc::client_sync::MessageType::ParameterUpdate:
-    return MSG_TYPE_PARAMETER_UPDATE;
+    return POINTRECEIVER_MSG_TYPE_PARAMETER_UPDATE;
   case pc::client_sync::MessageType::ParameterRequest:
-    return MSG_TYPE_PARAMETER_REQUEST;
-  default: return MSG_TYPE_UNKNOWN;
+    return POINTRECEIVER_MSG_TYPE_PARAMETER_REQUEST;
+  default: return POINTRECEIVER_MSG_TYPE_UNKNOWN;
   }
 }
 
+#ifdef __ANDROID__
+void log(std::string text) {
+  auto text_cstr = text.c_str();
+  int (*alias)(int, const char *, const char *, ...) = __android_log_print;
+  alias(ANDROID_LOG_VERBOSE, APPNAME, text_cstr);
+}
+#else
+template <typename... Args>
+void log(fmt::format_string<Args...> fmt, Args &&...args) {
+  spdlog::info(fmt, std::forward<Args>(args)...);
+}
+#endif
+
 extern "C" {
 
-PointReceiverContext *createPointReceiverContext() {
-  return new PointReceiverContext();
+pointreceiver_context *pointreceiver_create_context() {
+  return new pointreceiver_context();
 }
 
-void destroyPointReceiverContext(PointReceiverContext *ctx) {
+void pointreceiver_destroy_context(pointreceiver_context *ctx) {
   if (ctx) {
     if (ctx->message_thread) { ctx->message_thread->request_stop(); }
   }
   delete ctx;
 }
 
-int startMessageReceiver(PointReceiverContext *ctx,
-                         const char *pointcaster_address) {
+int pointreceiver_start_message_receiver(pointreceiver_context *ctx,
+                                         const char *pointcaster_address) {
   // initialise the zmq context if it hasn't already been initialised
   std::call_once(zmq_ctx_once,
                  [] { zmq_ctx = std::make_unique<zmq::context_t>(); });
@@ -98,9 +119,9 @@ int startMessageReceiver(PointReceiverContext *ctx,
           return;
         }
 
-        // TODO implement byte enums shared over pointcaster and receiver
-        uint8_t connected = 0x00;
-        zmq::message_t msg(&connected, sizeof(connected));
+        // Send our server a connected message
+        constexpr auto connected_signal = pc::client_sync::MessageType::Connected;
+        zmq::message_t msg(&connected_signal, sizeof(connected_signal));
         socket.send(msg, zmq::send_flags::none);
 
         log("Listening for messages at '{}'", endpoint);
@@ -116,11 +137,15 @@ int startMessageReceiver(PointReceiverContext *ctx,
           auto bytes_ptr = static_cast<std::byte *>(incoming_msg.data());
           buffer.assign(bytes_ptr, bytes_ptr + msg_size);
 
-          SyncMessage message;
           zpp::bits::in in(buffer);
-          auto deserialise_result = in(message);
-          // TODO test result for failure
-          ctx->message_queue.try_enqueue(message);
+          SyncMessage message;
+          if (failure(in(message))) {
+            log("Failed to deserialise incoming message from Pointcaster");
+            continue;
+          }
+          if (!ctx->message_queue.try_enqueue(message)) {
+            log("Failed to enqueue incoming message from Pointcaster");
+          }
         }
 
         socket.disconnect(endpoint);
@@ -130,7 +155,7 @@ int startMessageReceiver(PointReceiverContext *ctx,
   return 0;
 }
 
-int stopMessageReceiver(PointReceiverContext *ctx) {
+int pointreceiver_stop_message_receiver(pointreceiver_context *ctx) {
   if (ctx->message_thread) {
     ctx->message_thread->request_stop();
     ctx->message_thread.reset();
@@ -138,8 +163,9 @@ int stopMessageReceiver(PointReceiverContext *ctx) {
   return 0;
 }
 
-bool dequeueMessage(PointReceiverContext *ctx,
-                    PointReceiverSyncMessage *out_message, int timeout_ms) {
+bool pointreceiver_dequeue_message(pointreceiver_context *ctx,
+                                   pointreceiver_sync_message *out_message,
+                                   int timeout_ms) {
   if (!ctx || !out_message) return false;
 
   using namespace pc::client_sync;
@@ -155,10 +181,10 @@ bool dequeueMessage(PointReceiverContext *ctx,
     auto message_type = std::get<pc::client_sync::MessageType>(message);
     out_message->message_type = convertMessageType(message_type);
     out_message->id[0] = '\0';
-    out_message->value_type = PARAM_VALUE_UNKNOWN;
+    out_message->value_type = POINTRECEIVER_PARAM_VALUE_UNKNOWN;
   } else if (std::holds_alternative<ParameterUpdate>(message)) {
     const auto &param_update = std::get<ParameterUpdate>(message);
-    out_message->message_type = MSG_TYPE_PARAMETER_UPDATE;
+    out_message->message_type = POINTRECEIVER_MSG_TYPE_PARAMETER_UPDATE;
     // Copy the id (ensure we don't overflow our fixed-size buffer)
     std::strncpy(out_message->id, param_update.id.c_str(), 256);
     out_message->id[255] = '\0';
@@ -168,23 +194,23 @@ bool dequeueMessage(PointReceiverContext *ctx,
         [&](const auto &val) {
           using T = std::decay_t<decltype(val)>;
           if constexpr (std::is_same_v<T, float>) {
-            out_message->value_type = PARAM_VALUE_FLOAT;
+            out_message->value_type = POINTRECEIVER_PARAM_VALUE_FLOAT;
             out_message->value.float_val = val;
           } else if constexpr (std::is_same_v<T, int>) {
-            out_message->value_type = PARAM_VALUE_INT;
+            out_message->value_type = POINTRECEIVER_PARAM_VALUE_INT;
             out_message->value.int_val = val;
           } else if constexpr (std::is_same_v<T, pc::types::Float3>) {
-            out_message->value_type = PARAM_VALUE_FLOAT3;
+            out_message->value_type = POINTRECEIVER_PARAM_VALUE_FLOAT3;
             out_message->value.float3_val.x = val.x;
             out_message->value.float3_val.y = val.y;
             out_message->value.float3_val.z = val.z;
           } else if constexpr (std::is_same_v<T, std::array<float, 3>>) {
-            out_message->value_type = PARAM_VALUE_FLOAT3;
+            out_message->value_type = POINTRECEIVER_PARAM_VALUE_FLOAT3;
             out_message->value.float3_val.x = val[0];
             out_message->value.float3_val.y = val[1];
             out_message->value.float3_val.z = val[2];
           } else {
-            out_message->value_type = PARAM_VALUE_UNKNOWN;
+            out_message->value_type = POINTRECEIVER_PARAM_VALUE_UNKNOWN;
           }
         },
         param_update.value);
@@ -195,7 +221,8 @@ bool dequeueMessage(PointReceiverContext *ctx,
 }
 
 // Start a thread that handles networking for the point cloud
-int startPointcloudReceiver(const char *pointcaster_address, int timeout_ms) {
+int pointrecevier_start_point_receiver(const char *pointcaster_address,
+                                       int timeout_ms) {
 
   // initialise the zmq context if it hasn't already been initialised
   std::call_once(zmq_ctx_once,
@@ -263,7 +290,7 @@ int startPointcloudReceiver(const char *pointcaster_address, int timeout_ms) {
   return 0;
 }
 
-int stopPointcloudReceiver() {
+int pointreceiver_stop_point_receiver() {
   if (pointcloud_thread) {
     pointcloud_thread->request_stop();
     pointcloud_thread.reset();
@@ -273,19 +300,23 @@ int stopPointcloudReceiver() {
 
 static PointCloud point_cloud;
 
-bool dequeuePointcloud(int timeout_ms) {
+bool pointreceiver_dequeue_points(int timeout_ms) {
   point_cloud = PointCloud{};
   return cloud_queue.wait_dequeue_timed(point_cloud,
                                         std::chrono::milliseconds(timeout_ms));
 }
 
-int pointCount() { return point_cloud.size(); }
+int pointreceiver_point_count() { return point_cloud.size(); }
+
+// TODO make below compatible with C api
+
 bob::types::color *pointColors() { return point_cloud.colors.data(); }
 
 bob::types::position *pointPositions() { return point_cloud.positions.data(); }
 
 std::vector<std::array<float, 4>> shader_friendly_point_positions_data;
 
+enum class OrientationType { unchanged = 0, flip_x, flip_x_z, flip_z };
 float *shaderFriendlyPointPositions(bool parallel_transform,
                                     OrientationType orientation_type) {
   std::size_t size = point_cloud.positions.size();
@@ -323,19 +354,19 @@ float *shaderFriendlyPointPositions(bool parallel_transform,
 }
 }
 
-void testMessageLoop(PointReceiverContext *ctx) {
+void testMessageLoop(pointreceiver_context *ctx) {
   int count = 0;
   while (count++ < 1000) {
-    PointReceiverSyncMessage msg;
-    // Try to dequeue a message with a 5-millisecond timeout.
-    if (dequeueMessage(ctx, &msg, 5)) {
-      if (msg.message_type == MSG_TYPE_PARAMETER_UPDATE) {
+    pointreceiver_sync_message msg;
+    // dequeue with a 5-millisecond timeout.
+    if (pointreceiver_dequeue_message(ctx, &msg, 5)) {
+      if (msg.message_type == POINTRECEIVER_MSG_TYPE_PARAMETER_UPDATE) {
         log("Parameter Update received: id = {}, value_type = {}", msg.id,
             (int)msg.value_type);
         switch (msg.value_type) {
-        case PARAM_VALUE_FLOAT: log("Value: {}", msg.value.float_val); break;
-        case PARAM_VALUE_INT: log("Value: {}", msg.value.int_val); break;
-        case PARAM_VALUE_FLOAT3:
+        case POINTRECEIVER_PARAM_VALUE_FLOAT: log("Value: {}", msg.value.float_val); break;
+        case POINTRECEIVER_PARAM_VALUE_INT: log("Value: {}", msg.value.int_val); break;
+        case POINTRECEIVER_PARAM_VALUE_FLOAT3:
           log("Value: ({}, {}, {})", msg.value.float3_val.x,
               msg.value.float3_val.y, msg.value.float3_val.z);
           break;
@@ -349,33 +380,33 @@ void testMessageLoop(PointReceiverContext *ctx) {
   }
 }
 
-void testPointcloudLoop() {
-  int i = 0;
-  while (i++ < 6000) {
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(0.5ms);
-    if (!dequeuePointcloud()) continue;
-    auto count = pointCount();
-    // auto buffer = pointPositions();
-    log("--{} points recieved", count);
-    // auto o = buffer[200];
-    // log(fmt::format("x {}, y {}, z {}, p {}", o.x, o.y, o.z, o.__pad));
-  }
-}
+// void testPointcloudLoop() {
+//   int i = 0;
+//   while (i++ < 6000) {
+//     using namespace std::chrono_literals;
+//     std::this_thread::sleep_for(0.5ms);
+//     if (!dequeuePointcloud(5)) continue;
+//     auto count = pointCount();
+//     // auto buffer = pointPositions();
+//     log("--{} points recieved", count);
+//     // auto o = buffer[200];
+//     // log(fmt::format("x {}, y {}, z {}, p {}", o.x, o.y, o.z, o.__pad));
+//   }
+// }
 
 int main(int argc, char *argv[]) {
-  auto *ctx = createPointReceiverContext();
+  auto *ctx = pointreceiver_create_context();
   if (argc < 2) {
-    startPointcloudReceiver();
-    startMessageReceiver(ctx, "127.0.0.1");
+    // pointreceiver_start_point_receiver("127.0.0.1", 5);
+    pointreceiver_start_message_receiver(ctx, "127.0.0.1");
   } else {
-    startPointcloudReceiver(argv[1]);
-    startMessageReceiver(ctx, argv[1]);
+    // pointreceiver_start_point_receiver(argv[1], 5);
+    pointreceiver_start_message_receiver(ctx, argv[1]);
   }
   // for (int i = 0; i < 3; i++) { testPointcloudLoop(); }
   for (int i = 0; i < 3; i++) { testMessageLoop(ctx); }
-  stopPointcloudReceiver();
-  stopMessageReceiver(ctx);
-  destroyPointReceiverContext(ctx);
+  pointreceiver_stop_message_receiver(ctx);
+  // pointreciever_stop_point_receiver();
+  pointreceiver_destroy_context(ctx);
   return 0;
 }
