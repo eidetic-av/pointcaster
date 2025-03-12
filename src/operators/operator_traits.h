518f4da81d89ee4df5647219d876f632799fd1ec
Matt Hughes <matth@eidetic.net.au>
2025-03-12 17:33:13 +1100

    add operator trait for checking if a config has the 'draw' field

--- src/operators/operator_traits.h
+++ src/operators/operator_traits.h
@@ -5,13 +5,17 @@
 namespace pc::operators {
 
 template <typename T, typename Scene, typename Group>
-concept RequiresInit = requires(T config, Scene &scene, Group &group) {
+concept requires_init = requires(T config, Scene &scene, Group &group) {
   { T::init(config, scene, group) } -> std::same_as<void>;
 };
 
+template <typename, typename = std::void_t<>>
+struct has_draw : std::false_type {};
+
 template <typename T>
-concept ToggleableDrawing = requires(const T &t) {
-  { t.draw } -> std::convertible_to<bool>;
-};
+struct has_draw<T, std::void_t<decltype(std::declval<T>().draw)>>
+    : std::true_type {};
+
+template <typename T> inline constexpr bool has_draw_v = has_draw<T>::value;
 
 } // namespace pc::operators
