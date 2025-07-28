4f25436f510a216c38ee4ebaf6e5d626c8a6cb2b
Matt Hughes <matth@eidetic.net.au>
2025-07-29 09:31:45 +1000

    bump corrade

--- ports/corrade/portfile.cmake
+++ ports/corrade/portfile.cmake
@@ -1,12 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mosra/corrade
-    REF 295bbba1f49887da060465f88b8501965f6acd7d
-    SHA512 58af843018979bb7a7ea9a41f2655a81e6c64e51dbc08c64cc3bc194c38bd23e6d4dc5169773d8312579954b70cc01b05558e0f0adb69ef874408a6a0cb3b694
+    REF f37591439479a5169134f8f2eba551b95054057a
+    SHA512 268c569c6290e4bed7a5fff38de0bd30b013dab090e08f2d9df75c192e0403eaa5f99cc8288a0eaab9995bce58181d24d86cebf3800de2281b55dfb3f80cb336
     HEAD_REF master
-    PATCHES
-    # fix-vs2019.patch
-      build-corrade-rc-always.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
@@ -19,7 +16,7 @@ foreach(_feature IN LISTS ALL_FEATURES)
     string(REPLACE "-" "_" _FEATURE "${_FEATURE}")
 
     # Final feature is empty, ignore it
-    if(_feature)
+    if(_feature AND NOT "${_feature}" STREQUAL "dynamic-pluginmanager")
         list(APPEND _COMPONENTS ${_feature} WITH_${_FEATURE})
     endif()
 endforeach()
@@ -40,9 +37,9 @@ vcpkg_cmake_configure(
         "${corrade_rc_param}"
         -DUTILITY_USE_ANSI_COLORS=ON
         -DBUILD_STATIC=${BUILD_STATIC}
-        -DCORRADE_CPU_USE_IFUNC=OFF
     MAYBE_UNUSED_VARIABLES
         CORRADE_RC_EXECUTABLE
+        UTILITY_USE_ANSI_COLORS
 )
 
 vcpkg_cmake_install()
@@ -52,13 +49,11 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # corrade-rc is not built when CMAKE_CROSSCOMPILING
-if("utility" IN_LIST FEATURES)
-    vcpkg_copy_tools(TOOL_NAMES "corrade-rc" AUTO_CLEAN)
-endif()
+vcpkg_copy_tools(TOOL_NAMES "corrade-rc" AUTO_CLEAN)
 
 # Ensure no empty folders are left behind
-if(NOT FEATURES)
-    # No features, no binaries (only Corrade.h).
+if(FEATURES STREQUAL "core")
+    # No features, no libs (only Corrade.h).
     file(REMOVE_RECURSE
         "${CURRENT_PACKAGES_DIR}/bin"
         "${CURRENT_PACKAGES_DIR}/lib"
