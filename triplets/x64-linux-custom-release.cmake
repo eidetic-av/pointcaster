681d500fbe1ce8dd542ad7d1d55454208a691356
Matt Hughes <matth@eidetic.net.au>
2026-05-20 22:54:04 +1000

    add release optimisations for vcpkg dependencies

--- triplets/x64-linux-custom-release.cmake
+++ triplets/x64-linux-custom-release.cmake
@@ -1,3 +1,7 @@
 set(VCPKG_BUILD_TYPE release)
 
 include(${CMAKE_CURRENT_LIST_DIR}/x64-linux-custom.cmake)
+
+set(VCPKG_CXX_FLAGS_RELEASE "-O3 -march=skylake-avx512 -ffast-math")
+set(VCPKG_C_FLAGS_RELEASE "-O3 -march=skylake-avx512 -ffast-math")
+
