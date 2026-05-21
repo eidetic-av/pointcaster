0625bd6987de35c5151bb20cb6f33c6e6d532579
Matt Hughes <matth@eidetic.net.au>
2026-05-21 16:41:30 +1000

    rename base vcpkg triplet for all build presets

--- triplets/x64-linux-custom.cmake
+++ triplets/x64-linux-custom.cmake
@@ -3,4 +3,4 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-include(${CMAKE_CURRENT_LIST_DIR}/x64-all-linkage.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/x64-all-platforms.cmake)
--- triplets/x64-windows-static-md-custom.cmake
+++ triplets/x64-windows-static-md-custom.cmake
@@ -2,4 +2,4 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-include(${CMAKE_CURRENT_LIST_DIR}/x64-all-linkage.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/x64-all-platforms.cmake)
