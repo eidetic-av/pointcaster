6e1bb8fc82aa001fa540e51d319a44ae9be32f45
Matt Hughes <matth@eidetic.net.au>
2026-05-21 16:42:15 +1000

    compile release builds to 'x86-64-v3' instead of avx512 so that zen3 chips work

--- triplets/x64-linux-custom-release.cmake
+++ triplets/x64-linux-custom-release.cmake
@@ -2,11 +2,12 @@ set(VCPKG_BUILD_TYPE release)
 
 include(${CMAKE_CURRENT_LIST_DIR}/x64-linux-custom.cmake)
 
-set(VCPKG_CXX_FLAGS_RELEASE "-O3 -march=skylake-avx512 -ffast-math")
-set(VCPKG_C_FLAGS_RELEASE "-O3 -march=skylake-avx512 -ffast-math")
+set(VCPKG_CXX_FLAGS_RELEASE "-O3 -march=x86-64-v3 -ffast-math")
+set(VCPKG_C_FLAGS_RELEASE "-O3 -march=x86-64-v3 -ffast-math")
 
 if(PORT MATCHES "opencv4")
     set(VCPKG_CMAKE_CONFIGURE_OPTIONS
+        ${VCPKG_CMAKE_CONFIGURE_OPTIONS}
         -DENABLE_FAST_MATH=ON
         -DCUDA_FAST_MATH=ON
     )
--- triplets/x64-windows-static-md-custom-release.cmake
+++ triplets/x64-windows-static-md-custom-release.cmake
@@ -2,8 +2,8 @@ set(VCPKG_BUILD_TYPE release)
 
 include(${CMAKE_CURRENT_LIST_DIR}/x64-windows-static-md-custom.cmake)
 
-set(VCPKG_CXX_FLAGS_RELEASE "/O2 /arch:AVX512 /fp:fast")
-set(VCPKG_C_FLAGS_RELEASE "/O2 /arch:AVX512 /fp:fast")
+set(VCPKG_CXX_FLAGS_RELEASE "/O2 /arch:AVX2 /fp:fast")
+set(VCPKG_C_FLAGS_RELEASE "/O2 /arch:AVX2 /fp:fast")
 set(VCPKG_LINKER_FLAGS_RELEASE "/LTCG")
 
 if(PORT MATCHES "opencv4")
