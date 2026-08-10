6bfadfd66caeaf06d9a4278a80418aa3ddb37f3f
Matt Hughes <matth@eidetic.net.au>
2026-08-11 02:44:55 +1000

    version 0.2.4

--- src/pointcaster.cc
+++ src/pointcaster.cc
@@ -14,7 +14,7 @@
 using namespace pc;
 
 int main(int argc, char *argv[]) {
-  std::println("This is pointcaster 0.2.3");
+  std::println("This is pointcaster 0.2.4");
 
   auto *app_settings = AppSettings::instance();
 
