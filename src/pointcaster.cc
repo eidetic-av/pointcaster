7242633364e2d3ea7feb3af0d847045ba52c190a
Matt Hughes <git@mail.matth.cc>
2026-09-08 23:57:38 +1000

    version 0.2.13

--- src/pointcaster.cc
+++ src/pointcaster.cc
@@ -14,7 +14,7 @@
 using namespace pc;
 
 int main(int argc, char *argv[]) {
-  std::println("This is pointcaster 0.2.12");
+  std::println("This is pointcaster 0.2.13");
 
   auto *app_settings = AppSettings::instance();
 
